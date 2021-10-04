9f90ab7ceca45a39b48a49e4928b187f62c967c9
harisyahfajars9@gmail.com <harisyahfajars9@gmail,com>
2021-10-04 11:59:22 +0800

    profile dan your review

--- src/Screen/Profile/Profile.js
+++ src/Screen/Profile/Profile.js
@@ -1,10 +1,71 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React from "react";
+import { Text, View, StyleSheet, Image } from "react-native";
+import { Avatar, Input, Button } from "react-native-elements";
+import { ms } from "react-native-size-matters";
 
-export default function Profile() {
+const Profile = () => {
     return (
-        <View>
-            <Text></Text>
+        <View style={styles.fullScreen}>
+            <Avatar
+                containerStyle={{ marginTop: ms(20), marginBottom: ms(52) }}
+                size={"xlarge"}
+                rounded
+                source={{
+                    uri: "https://randomuser.me/api/portraits/women/57.jpg"
+                }}
+            >
+                <Avatar.Accessory style={styles.avatarIcon} size={40} />
+            </Avatar>
+            <Input
+                placeholder="Fullname"
+                inputStyle={{ color: "white" }}
+                inputContainerStyle={{ borderColor: "white" }}
+            />
+            <Input
+                placeholder="Username"
+                inputStyle={{ color: "white" }}
+                inputContainerStyle={{ borderColor: "white" }}
+            />
+            <Input
+                placeholder="Email"
+                inputStyle={{ color: "white" }}
+                inputContainerStyle={{ borderColor: "white" }}
+            />
+            <Input
+                placeholder="Password"
+                secureTextEntry={true}
+                inputStyle={{ color: "white" }}
+                inputContainerStyle={{ borderColor: "white" }}
+            />
+            <Button
+                title="LOGOUT"
+                buttonStyle={{
+                    backgroundColor: "white",
+                    width: ms(120),
+                    borderColor: "white",
+                    borderRadius: ms(22),
+                    height: ms(48)
+                }}
+                containerStyle={{
+                    alignItems: "center",
+                    marginVertical: ms(40)
+                }}
+                titleStyle={{ color: "black", fontWeight: "bold" }}
+            />
         </View>
-    )
-}
+    );
+};
+
+const styles = StyleSheet.create({
+    fullScreen: {
+        backgroundColor: "black",
+        flex: ms(1),
+        paddingHorizontal: ms(20),
+        alignItems: "center"
+    },
+    avatarIcon: {
+        backgroundColor: "#FDC600"
+    }
+});
+
+export default Profile;
