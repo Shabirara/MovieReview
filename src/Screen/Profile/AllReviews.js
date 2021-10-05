795312f07da0bff7c678d887c073637b338c1252
harisyahfajars9@gmail.com <harisyahfajars9@gmail,com>
2021-10-05 13:24:15 +0800

    fix profile

--- src/Screen/Profile/AllReviews.js
+++ src/Screen/Profile/AllReviews.js
@@ -1,120 +1,121 @@
-import axios from 'axios';
-import React, { useEffect, useState } from 'react'
-import { View, ScrollView, StyleSheet, TouchableOpacity } from 'react-native'
-import { Icon, Card, Avatar, Text } from 'react-native-elements'
-import { useSelector } from 'react-redux';
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { View, ScrollView, StyleSheet, TouchableOpacity } from "react-native";
+import { Icon, Card, Avatar, Text } from "react-native-elements";
+import { useSelector } from "react-redux";
 
 export default function AllReviews() {
-    const data = useSelector((state) => state.HomeReducer.MovieDetail)
-    const [user, setUser] = useState([{}])
-    const [dataUser, setDataUser] = useState([])
+  const data = useSelector((state) => state.HomeReducer.MovieDetail);
+  const [user, setUser] = useState([{}]);
+  const [dataUser, setDataUser] = useState([]);
 
-    return (
-        <>
-            <ScrollView style={styles.background}>
-                {data.map((e, i) => {
-                    return (
-                        <Card containerStyle={{ borderRadius: 20 }}>
-                            <View style={styles.container}>
+  return (
+    <>
+      <ScrollView style={styles.background}>
+        {data.map((e, i) => {
+          return (
+            <Card containerStyle={{ borderRadius: 20 }}>
+              <View style={styles.container}>
+                <Avatar
+                  rounded
+                  size="medium"
+                  source={{
+                    uri: "https://i.ytimg.com/vi/1Ne1hqOXKKI/maxresdefault.jpg",
+                  }}
+                />
 
-                                <Avatar rounded size="medium" source={{ uri: 'https://i.ytimg.com/vi/1Ne1hqOXKKI/maxresdefault.jpg' }} />
+                <View style={styles.header}>
+                  <View style={styles.review}>
+                    <Icon name="star" color="#F0CA02" />
+                    <Text style={styles.userReview}>{e.rating * 2}</Text>
+                    <Text style={styles.perTen}>/10</Text>
+                    <Text h2 style={styles.title}>
+                      Review
+                    </Text>
+                  </View>
 
-                                <View style={styles.header}>
+                  <View style={styles.reviewer}>
+                    <Text>Reviewer: </Text>
+                    <Text style={styles.user}>{e.User.fullName}</Text>
+                  </View>
+                </View>
+              </View>
 
-                                    <View style={styles.review}>
-                                        <Icon name='star' color='#F0CA02' />
-                                        <Text style={styles.userReview}>{e.rating * 2}</Text>
-                                        <Text style={styles.perTen}>/10</Text>
-                                        <Text h2 style={styles.title}>Review</Text>
-
-                                    </View>
-
-                                    <View style={styles.reviewer}>
-                                        <Text>Reviewer: </Text>
-                                        <Text style={styles.user}>{e.User.fullName}</Text>
-                                    </View>
-
-                                </View>
-
-                            </View>
-
-                            <Text>{e.comment}</Text>
-
-                        </Card>
-                    )
-                })}
-
-            </ScrollView>
-            <TouchableOpacity style={styles.fab}>
-                <Icon name='add' size={60} color='#FEC100' />
-            </TouchableOpacity>
-        </>
-    )
+              <Text>{e.comment}</Text>
+            </Card>
+          );
+        })}
+      </ScrollView>
+      <TouchableOpacity style={styles.fab}>
+        <Icon name="add" size={60} color="#FEC100" />
+      </TouchableOpacity>
+    </>
+  );
 }
 
 const styles = StyleSheet.create({
-    background: {
-        backgroundColor: "black",
-        flex: 1
-    },
+  background: {
+    backgroundColor: "black",
+    flex: 1,
+  },
 
-    container: {
-        flexDirection: "row",
-        paddingVertical: 10,
-        alignItems: "center",
-    },
+  container: {
+    flexDirection: "row",
+    paddingVertical: 10,
+    alignItems: "center",
+  },
 
-    header: {
-        flexDirection: "column",
-        paddingHorizontal: 10
-    },
+  header: {
+    flexDirection: "column",
+    paddingHorizontal: 10,
+  },
 
-    review: {
-        flexDirection: "row",
-        alignItems: "center"
-    },
+  review: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
 
-    userReview: {
-        fontWeight: 'bold',
-        fontSize: 18
-    },
+  userReview: {
+    fontWeight: "bold",
+    fontSize: 18,
+  },
 
-    perTen: {
-        fontSize: 18
-    },
+  perTen: {
+    fontSize: 18,
+  },
 
-    reviewer: {
-        flexDirection: "row"
-    },
+  reviewer: {
+    flexDirection: "row",
+  },
 
-    title: {
-        paddingHorizontal: 10
-    },
+  title: {
+    paddingHorizontal: 10,
+  },
 
-    user: {
-        fontWeight: "bold"
-    },
+  user: {
+    fontWeight: "bold",
+  },
 
-    containerFab: {
-        flex: 1,
-        backgroundColor: 'red',
-        position: 'absolute',
-        bottom: 0
-    },
+  containerFab: {
+    flex: 1,
+    backgroundColor: "red",
+    position: "absolute",
+    bottom: 0,
+  },
 
-    fab: {
-        borderWidth: 1,
-        borderColor: 'rgba(0,0,0,0.2)',
-        alignItems: 'center',
-        justifyContent: 'center',
-        width: 70,
-        position: 'absolute',
-        bottom: 30,
-        right: 30,
-        height: 70,
-        backgroundColor: '#824E00',
-        borderRadius: 100,
-    },
+  fab: {
+    borderWidth: 1,
+    borderColor: "rgba(0,0,0,0.2)",
+    alignItems: "center",
+    justifyContent: "center",
+    width: 70,
+    position: "absolute",
+    bottom: 30,
+    right: 30,
+    height: 70,
+    backgroundColor: "#824E00",
+    borderRadius: 100,
+  },
 });
 
 // {dataFilm?.map((value, index) => (
@@ -134,7 +135,8 @@ const styles = StyleSheet.create({
 //   </View>
 // ))}
 
-{/* <FAB
+{
+  /* <FAB
         style={styles.fab}
         large
         icon={
@@ -145,4 +147,5 @@ const styles = StyleSheet.create({
           />
         }
         color="#824E00"
-      /> */}
+      /> */
+}
